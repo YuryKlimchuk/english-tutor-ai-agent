7a6e2033c60b13b2ae8db25748bfd96fa233bd02
yury.klimchuk <hydro.yura@gmail.com>
2026-06-15 22:19:39 +0300

    init spring boot project

---
+++ src/main/java/com/hydroyura/eta/EtaApplication.java
@@ -0,0 +1,13 @@
+package com.hydroyura.eta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EtaApplication.class, args);
+	}
+
+}
